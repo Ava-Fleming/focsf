042cd1bc6321e24c5985c6a0a02ed6b37c3d56c2
Ava Fleming <devpump@gmail.com>
2015-02-16 20:55:27 +0000

    Added local bootstrap and jquery files to fix non-existing errors Added ajax call for event html template.

--- js/master.js
+++ js/master.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   /*TODO: On load, pull top 10 items close to school*/
   var TESTBTN = $('#TESTBTN');
-
+  var eventDetails = $('#eventDetails');
   function initialize(mapLatLong) {
+    /*Sets map options*/
     var mapOptions = {
       center: {lat: mapLatLong.LAT, lng: mapLatLong.LNG},
       zoom: 8
     };
+    /*Places the google map in the map-canvas with the given options*/
     var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
+    /*Inserts a pin or marker in a specfic latitude and longitude.*/
     var marker = new google.maps.Marker({
       position: {lat: mapLatLong.LAT, lng: mapLatLong.LNG},
       map: map
@@ -16,12 +19,17 @@ $(document).ready(function(){
   }
   /*Button to throw test code in.*/
   TESTBTN.on('click',function(){
-    /*Ajax for latest events*/
-/*Object containing Latitude and Longitude points*/
-var mapLatLongCordinates = Object.create(null);
-mapLatLongCordinates.LAT = 28.1338075;
-mapLatLongCordinates.LNG = -81.95501380000002;
-initialize(mapLatLongCordinates);
+    /*Object containing Latitude and Longitude points*/
+    var mapLatLongCordinates = Object.create(null);
+    mapLatLongCordinates.LAT = 28.1338075;
+    mapLatLongCordinates.LNG = -81.95501380000002;
+    initialize(mapLatLongCordinates);
+    $.ajax({
+      url: 'templates/eventdetails.html'
+    }).done(function(returnedHTMLTemplate){
+      alert('hi');
+      console.log(returnedHTMLTemplate);
+    });
   });
 
 });
