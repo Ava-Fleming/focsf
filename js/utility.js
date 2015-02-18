e8a89a8ab495489abdb21963fb79c15729dc610f
Matt Fleming (DevPump) <devpump@gmail.com>
2015-02-18 18:49:52 +0000

    Removed test data on index Added globalServices and JSON2Text identifiers in utility.js Properly return a stored procedure and requested data for index. Muliple markers are now working.

---
+++ js/utility.js
@@ -0,0 +1,15 @@
+var globalServices = Object.create(null);
+globalServices.selectEvents = "php/selectevents.php";
+globalServices.insertEvents = "php/insertevents.php";
+
+var JSON2Text = Object.create(null);
+JSON2Text.personName = 0;
+JSON2Text.eventName = 1;
+JSON2Text.eventDescription = 2;
+JSON2Text.location = 3;
+JSON2Text.latitude = 4;
+JSON2Text.longitude = 5;
+JSON2Text.startTime = 6;
+JSON2Text.endTime = 7;
+JSON2Text.hoursGiven = 8;
+JSON2Text.ExtraNotes = 9;
--- php/selectevents.php
+++ php/selectevents.php
@@ -3,21 +3,21 @@ require_once('config.php');
 
 // Check connection
 if (mysqli_connect_errno())
-  {
+{
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
 
-$sql="SELECT * FROM events";
-
+$sql="CALL Select_NewestEvents(". $_POST["maxEvents"] .")";
+$returnedData = null;
 if ($result=mysqli_query($a,$sql))
-  {
-  // Fetch one and one row
-  while ($row=mysqli_fetch_row($result))
-    {
-    printf ("%s (%s)\n",$row["personName"],$row["eventName"]);
-    }
+{
+  $datas = json_encode(mysqli_fetch_all($result));
+  echo $datas;
   // Free result set
   mysqli_free_result($result);
+}
+else{
+  echo "Nothing to do.";
 }
 
 mysqli_close($con);
