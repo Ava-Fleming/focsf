289e5dd7fe55477ce0cb06edd2e602e60321dccd
Matt Fleming (DevPump) <devpump@gmail.com>
2015-02-19 14:50:30 +0000

    blah

--- js/map.js
+++ js/map.js
@@ -13,9 +13,11 @@ function initializeMap(dataForMap){
   /*Places the google map in the map-canvas with the given options*/
   var markers = Array();
   var infoWindowContent = Array();
+  console.log(dataForMap.D.length);
   for(var i = 0; i< dataForMap.length; i++){
-    markers.push([dataForMap[i][JSON2Text.location], parseFloat(dataForMap[i][JSON2Text.latitude]),parseFloat(dataForMap[i][JSON2Text.longitude])]);
-    infoWindowContent.push('<div class="info_content"><h3>' + dataForMap[i][JSON2Text.eventName] + '</h3><p>' + dataForMap[i][JSON2Text.eventDescription] +'</p></div>');
+    console.log(dataForMap[i].Location);
+    markers.push([dataForMap[i].Location, parseFloat(dataForMap[i].Latitude),parseFloat(dataForMap[i].Longitude)]);
+    infoWindowContent.push('<div class="info_content"><h3>' + dataForMap[i].PersonName + '</h3><p>' + dataForMap[i].EventDescription +'</p></div>');
   }
   var infoWindow = new google.maps.InfoWindow(), marker, i;
   // Loop through our array of markers & place each one on the map
@@ -32,17 +34,17 @@ function initializeMap(dataForMap){
         infoWindow.setContent(infoWindowContent[i]);
         infoWindow.open(map, marker);
         /*Append text about point on map to event details list.*/
-        eventDetails.empty().append('<li>Name: ' + dataForMap[i][JSON2Text.personName] + '</li>');
-        eventDetails.append('<li>Event: ' + dataForMap[i][JSON2Text.eventName] + '</li>');
-        eventDetails.append('<li>Event Description: ' + dataForMap[i][JSON2Text.eventDescription] + '</li>');
-        eventDetails.append('<li>Event Location: ' + dataForMap[i][JSON2Text.location] + '</li>');
+        eventDetails.empty().append('<li>Name: ' + dataForMap[i].PersonName + '</li>');
+        eventDetails.append('<li>Event: ' + dataForMap[i].EventName + '</li>');
+        eventDetails.append('<li>Event Description: ' + dataForMap[i].EventDescription + '</li>');
+        eventDetails.append('<li>Event Location: ' + dataForMap[i].ocation + '</li>');
         /*TODO convert to normal time.*/
-        eventDetails.append('<li>Event Start Time: ' + dataForMap[i][JSON2Text.startTime] + '</li>');
-        eventDetails.append('<li>Event End Time: ' + dataForMap[i][JSON2Text.endTime] + '</li>');
-        eventDetails.append('<li>Hours Given: ' + dataForMap[i][JSON2Text.hoursGiven] + '</li>');
-        eventDetails.append('<li>Extra Notes: ' + dataForMap[i][JSON2Text.extraNotes] + '</li>');
-        console.log(dataForMap[i][JSON2Text.startTime]);
-        dateTimeParse(dataForMap[i][JSON2Text.startTime]);
+        eventDetails.append('<li>Event Start Time: ' + dataForMap[i].StartTime + '</li>');
+        eventDetails.append('<li>Event End Time: ' + dataForMap[i].EndTime + '</li>');
+        eventDetails.append('<li>Hours Given: ' + dataForMap[i].HoursGiven + '</li>');
+        eventDetails.append('<li>Extra Notes: ' + dataForMap[i].ExtraNotes + '</li>');
+        console.log(dataForMap[i].StartTime);
+        dateTimeParse(dataForMap[i].StartTime);
       }
     })(marker, i));
   }
