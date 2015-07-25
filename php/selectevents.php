9544e2cd28c6175005b4774e7aef958d4854e40f
DevPump <devpump@gmail.com>
2015-07-25 00:21:33 -0400

    Updated Bootstrap to 3.3.5; jQuery to 1.11.3; Removed uneeded code in map.js; Linked map.js to the proper sql tables.; Allowed selectevents.php to take a custom maxEvent number.; Prevent error on no maxEvents given to selectevents.php; Include the current schema;

--- php/selectevents.php
+++ php/selectevents.php
@@ -1,10 +1,11 @@
 <?php
 require_once('database.php');
 require_once('utility.php');
-// Check connection
-$maxEvents =  1; //$_POST["maxEvents"];
-$selectStatement = $mysqlConnection->prepare(SelectApprovedEvents()); //SelectApprovedEvents());
-$selectStatement->bindValue(':maxEvents', $maxEvents, PDO::PARAM_INT);
-/*Pull All requested in the stored procedure. */
-$selectStatement->execute();
-echo json_encode($selectStatement->fetchAll(PDO::FETCH_ASSOC));;
+if(!empty($_POST["maxEvents"])){
+  $maxEvents = $_POST["maxEvents"];
+  $selectStatement = $mysqlConnection->prepare(SelectApprovedEvents()); //SelectApprovedEvents());
+  $selectStatement->bindValue(':maxEvents', $maxEvents, PDO::PARAM_INT);
+  /*Pull All requested in the stored procedure. */
+  $selectStatement->execute();
+  echo json_encode($selectStatement->fetchAll(PDO::FETCH_ASSOC));;
+}
